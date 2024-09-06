3fd5de1f62f740976cd4a9cb6601146048e36cff
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:53:35 +0000

    write contacts to file

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const jsonData = JSON.stringify(updatedContacts, null, 2);
+
+    await fs.writeFile(PATH_DB, jsonData, 'utf-8');
+
+    console.log('Contacts successfully written to', PATH_DB);
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw error;
+  }
+};
