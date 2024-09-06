5ca4d9176944fb9d472986d12f6abcd39fcd2668
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:53:20 +0000

    remove last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
+export const removeLastContact = async () => {};
+try {
+  const contacts = await readContacts();
+  if (contacts.length > 0) {
+    contacts.length = contacts.length - 1;
+    await writeContacts(contacts);
+  }
+} catch (error) {
+  console.error('Error deleting last contact:', error);
+  throw error;
+}
 removeLastContact();
