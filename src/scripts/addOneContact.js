89036576336125f3ac3a57614177be2c1bace6c3
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:52:46 +0000

    +1 contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const newContact = createFakeContact();
+    const updatedContacts = [...existingContacts, newContact];
+    await writeContacts(updatedContacts);
+    console.log('New contact added successfully.');
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+};
 
 addOneContact();
