dccf37690227dda05a97bfe18e613dba36ea1c67
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:52:57 +0000

    quantity of contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,14 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    const count = contacts.length;
+    return count;
+  } catch (error) {
+    console.error('Error getting contacts:', error);
+    throw error;
+  }
+};
 
 console.log(await countContacts());
