f7f442c80c050f15bef62b34f6891dafd0c70478
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:53:12 +0000

    remove all contacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,15 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    contacts.length = 0;
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error('Error deleting all contacts:', error);
+    throw error;
+  }
+};
 
 removeAllContacts();
