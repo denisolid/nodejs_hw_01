55b56347092c4bd21306a67e5d05fd6c109f51bb
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:52:39 +0000

    generate contacts logic

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { PATH_DB } from './constants/contacts.js';
-import { readContacts } from './utils/readContacts.js';
-
-console.log(PATH_DB);
-
-console.log(readContacts());
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
