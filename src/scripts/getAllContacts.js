6365642e1ab1e7f65f6d5b79d07e3a5201553975
denisolid <dkolisnichienko321@gmail.com>
2024-09-06 19:53:04 +0000

    get contacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const existingContacts = await readContacts();
+    return existingContacts;
+  } catch (error) {
+    console.error('Error getting contacts:', error);
+    throw error;
+  }
+};
 
 console.log(await getAllContacts());
